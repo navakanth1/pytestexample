b8bf4f6b1ab945646baf451bf563676282fb4bad
navakanth1 <navakanthreddy4445@gmail.com>
2022-03-28 10:13:19 +0530

    update

--- test_Cal.py
+++ test_Cal.py
@@ -2,22 +2,22 @@ import Cal
 
 
 def test_add2no():
-    a = 10
-    b = 20
+    a = 19
+    b = 34
     result = Cal.add2no(a,b)
     assert result == a+b
 def test_sub2no():
-    a = 15
-    b = 14
+    a = 16
+    b = 1414
     result = Cal.sub2no(a,b)
     assert result == a-b
 def test_mul2no():
-    a = 54
-    b = 71
+    a = 22
+    b = 122
     result = Cal.mul2no(a,b)
     assert result == a*b
 def test_div2no():
-    a = 45
-    b = 10
+    a = 23
+    b = 12
     result = Cal.div2no(a,b)
     assert result == a/b
