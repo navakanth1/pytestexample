262ea5ce2a8882808c88d2c3b004020c681113df
navakanth1 <navakanthreddy4445@gmail.com>
2022-03-28 10:09:34 +0530

    update

--- Cal.py
+++ Cal.py
@@ -9,16 +9,18 @@ def mul2no(x, y):
 
 def div2no(x, y):
     return x/y
-a = int(input("enter 1st: "))
-b = int(input("enter 2nd: "))
 
 if __name__ == "__main__":
+    a = int(input("enter 1st: "))
+    b = int(input("enter 2nd: "))
+
     add = add2no(a, b)
     subtract = sub2no(a, b)
     product = mul2no(a, b)
     divison = div2no(a, b)
 
-print(add)
-print(subtract)
-print(product)
-print(divison)
+    print(add)
+    print(subtract)
+    print(product)
+    print(divison)
+
